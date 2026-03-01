fd237cf8e2f2a2970b5e1e7cc4664965d3339a27
2007ducanhdang <25023147@vnu.edu.vn>
2026-03-01 16:29:26 +0700

    them file C++

---
+++ Untitled-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Sửa lỗi 1: viết liền iostream
+
+using namespace std; // Khai báo này để dùng cout và endl thoải mái
+
+int main() {
+    // Sửa lỗi 2: Dùng nháy kép " " cho chuỗi văn bản
+    cout << "welcome to C++" << endl; 
+    return 0;
+}
